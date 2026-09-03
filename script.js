5e799cb9433b4b07669a0caa132c6d33aeecc1bb
Sampada Chaugule <sampadachougule1008@gmail.com>
2026-09-03 22:31:14 +0530

    Remove legacy portfolio assets

--- script.js
+++
@@ -1,1 +0,0 @@
-const menuBtn=document.querySelector('.menu-btn');const nav=document.querySelector('#nav');menuBtn.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('#nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
